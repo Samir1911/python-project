ca308e6613796bf271c9bd26dd1d151466b3c781
Samir Abdul <samirabdul19jr1a@gmail.com>
2021-11-19 13:12:09 +0530

    main.py,main1.py r sample scripts

---
+++ main.py
@@ -0,0 +1,19 @@
+n=input("enter Number")
+'''l=len(n)
+for i in range(l//2):
+	if(n[i]!=n[l-i-1]):
+		print("Not a palindrome")
+		i=0	
+		break
+if(i!=0):
+	print("It is a palindrome")'''
+
+
+if(n==n[::-1]):
+	print("It is Palindrome")
+else:
+	print("Not a Palindrome")
+
+
+		
+	
---
+++ main1.py
@@ -0,0 +1,6 @@
+n=int(input("enter no. "))
+n=n**(1/2)
+if(n//1==n/1):
+	print("Perfect square")
+else:
+	print("not a perfect square")
